a2155a0d48b448036d68157c4c3c5de1fc1359e7
homerice <wyh900207@126.com>
2017-08-28 17:56:10 +0800

    update `Package.swift`

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
 // Package manager
 import PackageDescription
 
+let versions = Version(0,0,0)..<Version(10,0,0)
+let urls = [
+    "https://github.com/PerfectlySoft/Perfect-HTTPServer.git"
+]
+
+
 let package = Package(
     name: "SwiftServerDemo",
-    dependencies: [
-        .Package(
-            url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
-            majorVersion: 2, minor: 0
-        )
-    ]
+    targets: [],
+    dependencies: urls.map { .Package(url: $0, versions: versions) }
 )
